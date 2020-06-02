9266756ede1e1e65d00fd62b2e2fd5316022bbc3
Vinicius Fernandes <vinicius.fernandes@bunzlbrasil.protcapad.com.br>
2020-06-02 17:44:09 -0300

    Novo

---
+++ Exponente/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exponente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int base1;
+            int expoente;
+            int total;
+
+            Console.WriteLine("Eai! Bora fazer uma conta exponencial?");
+
+            Console.Write("Digite o número base: ");
+            base1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o número exponencial: ");
+            expoente = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            int resultado = 1;
+
+            for (int i = 1; i <= expoente; i++)
+            {
+                resultado *= base1;
+
+                Console.WriteLine("O valor da conta é: " + resultado);              
+            }
+
+            Console.ReadLine();
+         
+            
+        }
+    }
+}
